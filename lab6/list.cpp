e00be2200ac4cda55e03cb14bfe6594a19fbb280
Jose C. Campos <jccava09@gmail.com>
2013-10-31 09:43:13 -0600

    list.cpp funca

--- lab6/list.cpp
+++ lab6/list.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 template <class T>
-bool palin(const list<T> &L){
-	bool bandera=true;
+bool palin(list<T> &L){
+	//bool bandera=true;
 	if (L.size()==1)
 		return true;
 	else{
-		while(L.size()!=1){
+		//cout<<primero<<"\t"<<ultimo<<endl;
+		while(L.size()>1){
 			T primero=L.front();
 			T ultimo=L.back();
-			if(primero=!ultimo){
-				bandera=false;
-				break;
+			if(primero!=ultimo){
+				return false;
 			}
 			L.pop_front();
 			L.pop_back();
@@ -24,10 +24,40 @@ bool palin(const list<T> &L){
 }
 
 int main(){
-	list<int> L;
-	for(int i=0;i<=5;++i)
+	list<int> L,L2,L3,L4;
+	bool resultado;
+	for(int i=0;i<=5;++i){
 		L.push_back(i);
-	for(int i=5;i<=0;--i)
+		//cout<<i<<endl;
+	}
+	for(int i=5;i>=0;--i){
 		L.push_back(i);
-	palin(L);
+		//cout<<i<<endl;
+	}
+	for(int i=0;i<=5;++i){
+		L2.push_back(i);
+		//cout<<i<<endl;
+	}
+	L3.push_back(1);
+	
+	L4.push_back(0);
+	L4.push_back(1);
+	L4.push_back(2);
+	L4.push_back(3);
+	L4.push_back(4);
+	L4.push_back(5);
+	L4.push_back(4);
+	L4.push_back(3);
+	L4.push_back(2);
+	L4.push_back(1);
+	L4.push_back(0);	
+	
+	resultado=palin(L);
+	cout<<"resultado L "<<resultado<<endl;
+	resultado=palin(L2);
+	cout<<"resultado L2 "<<resultado<<endl;
+	resultado=palin(L3);
+	cout<<"resultado L3 "<<resultado<<endl;
+	resultado=palin(L4);
+	cout<<"resultado L4 "<<resultado<<endl;
 }
