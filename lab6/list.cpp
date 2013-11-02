3435e5e00b5a6a68a9465c48853bf53895172c94
Jose C. Campos <jccava09@gmail.com>
2013-11-01 22:02:00 -0600

    find y lastof funcan

--- lab6/list.cpp
+++ lab6/list.cpp
@@ -1,31 +1,67 @@
 #include <iostream>
 #include <list>
+#include <string>
 
 using namespace std;
 
 template <class T>
-bool palin(list<T> &L){
+string palin(list<T> L){
 	//bool bandera=true;
 	if (L.size()==1)
-		return true;
+		return "True";
 	else{
 		//cout<<primero<<"\t"<<ultimo<<endl;
 		while(L.size()>1){
 			T primero=L.front();
 			T ultimo=L.back();
 			if(primero!=ultimo){
-				return false;
+				return "False";
 			}
 			L.pop_front();
 			L.pop_back();
 		}
-		return true;
+		return "True";
 	}
 }
 
+template <class T>
+string findLastOf(list<T> &L, T valor){
+	int contador=0;
+	int posicion=0;
+	typename list<T>::iterator it;
+	for(it=L.begin();it!=L.end();++it){
+		contador++;
+		if(valor==(*it))
+			posicion=contador;
+	}
+	if(posicion!=0){
+	return ("La ultima ocurrencia de "+to_string(valor)+" esta en la posicion "
+			+to_string(posicion));
+	}
+	else
+	return ("end()");
+}
+
+template <class T>
+string sequentialSearch(list<T> &L, T valor){
+	int contador=0;
+	int posicion=0;
+	typename list<T>::iterator it;
+	for(it=L.begin();it!=L.end();++it){
+		contador++;
+		if(valor==(*it)){
+			posicion=contador;
+			return ("La primera ocurrencia de "+to_string(valor)+
+			" esta en la posicion "+to_string(posicion));
+		}
+	}
+	return ("end()");
+}
+
+
 int main(){
 	list<int> L,L2,L3,L4;
-	bool resultado;
+	string resultado;
 	for(int i=0;i<=5;++i){
 		L.push_back(i);
 		//cout<<i<<endl;
@@ -53,11 +89,20 @@ int main(){
 	L4.push_back(0);	
 	
 	resultado=palin(L);
-	cout<<"resultado L "<<resultado<<endl;
+	cout<<"resultado L "+resultado<<endl;
 	resultado=palin(L2);
-	cout<<"resultado L2 "<<resultado<<endl;
+	cout<<"resultado L2 "+resultado<<endl;
 	resultado=palin(L3);
-	cout<<"resultado L3 "<<resultado<<endl;
+	cout<<"resultado L3 "+resultado<<endl;
 	resultado=palin(L4);
-	cout<<"resultado L4 "<<resultado<<endl;
+	cout<<"resultado L4 "+resultado<<endl;
+	
+	
+	cout<<findLastOf(L,0)<<endl;
+	cout<<findLastOf(L,5)<<endl;
+	cout<<findLastOf(L4,2)<<endl;
+	cout<<findLastOf(L4,9)<<endl;
+	cout<<sequentialSearch(L4,0)<<endl;
+	cout<<sequentialSearch(L4,9)<<endl;
+	cout<<sequentialSearch(L4,5)<<endl;
 }
