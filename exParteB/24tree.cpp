3ec6ff6a59f3c447eab37d685652ccfbff4cff88
Jose C. Campos <jccava09@gmail.com>
2013-12-04 22:12:09 -0600

    creo que la busqueda ya esta(sin probar)

---
+++ exParteB/24tree.cpp
@@ -0,0 +1,39 @@
+#include "24tree.hh" 
+
+template <class k,class x>
+twofourTree<k,x>::twofourTree(){
+	root = NULL;
+}
+
+template <class k,class x>
+twofourTree<k,x>::~twofourTree(){
+}
+
+template <class k,class x>
+bool twofourTree<k,x>::search(k key){
+	for(int i = 0;i<(int)root->node_keys.size();++i){
+		if(key==root->node_keys[i]){
+			return true;
+		}
+		else if(key<root->node_keys[i]){
+			recursive_search(key,root->node_children[key]);
+		}
+	}	
+}
+
+template <class k,class x>
+bool recursive_search(k key,twofourNode<k,x>* child){
+	for(int i = 0;i<(int)child->node_keys.size();++i){
+		if(child->node_keys.empty()==true){
+			return false;
+		}
+		else{
+			if(key==child->node_keys[i]){
+				return true;
+			}
+			else if(key<child->node_keys[i]){
+				recursive_search(key,child->node_children[key]);
+			}
+		}
+	}
+}
---
+++ exParteB/24tree.hh
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+template <class k,class x>
+struct twofourNode{
+	vector<k> node_keys;
+	map<k,x> node_values;
+	map<k,twofourNode<k,x>* > node_children;
+};
+
+template <class k,class x>
+class twofourTree{
+	public:
+		bool search(k);
+		void insert(k,x);
+		twofourTree();
+		~twofourTree();
+	
+	private:
+		twofourNode<k,x>* root;
+		bool recursive_search(k,twofourNode<k,x>*);
+};
